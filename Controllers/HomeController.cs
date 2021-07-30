c2e2e480fa227cd61e179c369736f1e561e730d8
mahabubsikder751975 <mahabubsikder751975@gmail.com>
2021-07-31 02:41:24 +0600

    added router traffic trend analysis

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,5 +42,10 @@ namespace IDTPDashboards.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult MonitorRoutes()
+        {            
+            return View();
+        }
     }
 }
